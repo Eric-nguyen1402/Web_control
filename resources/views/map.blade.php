5f7d18253a13dd00cdaa9ed88f939190ea3738d1
Eric Nguyen <62949169+Eric-nguyen1402@users.noreply.github.com>
2022-01-06 15:27:48 +0800

    Update map.blade.php

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <head>
-    <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyBMJDGNgsYTqSDjlOP249KXu55GyXJTrNk"></script>
+    <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key="></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> <!-- add thư viện cho nút nhất hoặc icon -->
