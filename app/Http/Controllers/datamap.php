d3c79ed6fddf5b3a348dcd1c609ae36588cc73ff
Eric Nguyen <skynguyen1402@gmail.com>
2022-01-06 15:13:40 +0800

    Update datamap.php

--- app/Http/Controllers/datamap.php
+++ app/Http/Controllers/datamap.php
@@ -12,12 +12,14 @@ use Illuminate\Support\Facades\Auth;
 class datamap extends Controller
 {
     public function update_datamap(Request $request){
-
         $data = data_map::latest('ID')->get();
+        $data1 = data_map::latest('ID')->first();
+        $data1 = $data1 -> ID;
+        if ($data1 >= 5000){
+            data_map::truncate();
+        }
         return response()->json($data);
-        
     }
-
     public function update_level(Request $request){
         $tank_level = move_control::where('id',1)->get(['level']);
         return response()->json($tank_level);
